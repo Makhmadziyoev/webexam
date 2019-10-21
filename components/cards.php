a331177daeda9bb9b3d37f99c68fecf5a54e4bb7
Ali <alijon_82mk@mail.ru>
2019-10-21 18:59:00 +0300

    Структура

---
+++ components/cards.php
@@ -0,0 +1,36 @@
+<section class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex flex-column flex-md-row justify-content-center align-items-center">
+                <div class="card mb-2 mr-md-2" style="width: 12rem;">
+                    <img src="../img/logo.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card mb-2 mr-md-2" style="width: 12rem;">
+                    <img src="../img/logo.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card mb-2 mr-md-2" style="width: 12rem;">
+                    <img src="../img/logo.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card" style="width: 12rem;">
+                    <img src="../img/logo.png" class="card-img-top" alt="...">
+                      <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ components/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-between">
+                <div>
+                    <p>Copyright</p>
+                </div>
+                <div class="d-flex flex-column">
+                    <p>Звони: 999999999</p>
+                    <p>или 9999999</p>
+                </div>
+                <div>
+                    г.Москва
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
